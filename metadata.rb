13dde21834420865b44e428999a17cca35cbd1f1
Will Salt <williamejsalt@gmail.com>
2016-07-15 16:26:27 +0100

    fixed foodcritic errors and added rubocop ignore for trivial offences

--- metadata.rb
+++ metadata.rb
@@ -1,2 +1,12 @@
 name 'aws-kms'
+maintainer 'Will Salt'
+maintainer_email 'williamejsalt@gmail.com'
+license 'Apache 2.0'
+description 'Provides a resource to decrypt secrets from AWS KMS'
+
 version '0.0.1'
+
+source_url 'https://github.com/willejs/chef-aws-kms'
+issues_url 'https://github.com/willejs/chef-aws-kms/issues'
+
+supports 'ubuntu'
